6abece352050e06b21fb946e336f12725a4c4339
Newton <newtoncw@gmail.com>
2021-07-28 21:36:02 -0300

    Implementação em aula

--- src/br/edu/utfpr/dv/fipe/MainWindow.java
+++ src/br/edu/utfpr/dv/fipe/MainWindow.java
@@ -6,12 +6,27 @@ import javax.swing.JFrame;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MainWindow {
 
@@ -106,23 +121,65 @@ public class MainWindow {
 		this.carregarMarcas();
 	}
 	
-	private String getContent(String url) {
-		return "";
+	private String getContent(String url) throws Exception {
+		CloseableHttpClient httpCliente = HttpClientBuilder.create().build();
+		HttpUriRequest request = RequestBuilder.get().setUri(url)
+				.setHeader("accept", "application/json").build();
+		CloseableHttpResponse response = httpCliente.execute(request);
+		
+		if(response.getStatusLine().getStatusCode() == 200) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			
+			return IOUtils.toString(reader);
+		} else {
+			throw new Exception("HTTP code: " + 
+					String.valueOf(response.getStatusLine().getStatusCode()));
+		}
 	}
 
 	private void carregarMarcas() {
-		
+		try {
+			String json = getContent("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+			
+			ObjectMapper mapper = new ObjectMapper();
+			List<Marca> marcas = mapper.readValue(json, 
+					new TypeReference<List<Marca>>() {});
+			
+			this.comboMarca.removeAllItems();
+			for(Marca m: marcas) {
+				this.comboMarca.addItem(m);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void carregarModelos(Marca marca) {
-		
+		try {
+			String json = getContent(
+					"https://parallelum.com.br/fipe/api/v1/carros/marcas/" +
+					String.valueOf(marca.getCodigo()) +	"/modelos");
+			
+			ObjectMapper mapper = new ObjectMapper();
+			ListaModelos modelos = mapper.readValue(json, ListaModelos.class);
+			
+			this.comboModelo.removeAllItems();
+			for(Modelo m : modelos.getModelos()) {
+				this.comboModelo.addItem(m);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void carregarAnos(Marca marca, Modelo modelo) {
-		
+		this.comboAno.removeAllItems();
 	}
 	
 	private void carregarValor(Marca marca, Modelo modelo, Ano ano) {
-		
+		this.labelValor.setText("");
 	}
 }
